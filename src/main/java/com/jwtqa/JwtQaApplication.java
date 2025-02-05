d28705b0ee40bab468a3002e5697c22ef2f1207d
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-05 22:02:07 +0700

    init project

---
+++ src/main/java/com/jwtqa/JwtQaApplication.java
@@ -0,0 +1,13 @@
+package com.jwtqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtQaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtQaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jwtqa/JwtQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtQaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
