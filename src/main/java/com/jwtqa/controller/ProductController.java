c03516bfdd14316aa305a595d894ccd15c1289cd
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-05 22:47:46 +0700

    product

--- src/main/java/com/jwtqa/controller/ProductController.java
+++ src/main/java/com/jwtqa/controller/ProductController.java
@@ -1,4 +1,52 @@
 package com.jwtqa.controller;
 
+import com.jwtqa.model.Product;
+import com.jwtqa.service.product.IProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@CrossOrigin("*")
 public class ProductController {
+
+    private final IProductService productService;
+
+    public ProductController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getProduct() {
+        List<Product> products = productService.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+//        return ResponseEntity.ok(products);
+    }
+    @PostMapping
+    public ResponseEntity<?> addProduct(@RequestBody final Product product) {
+        productService.save(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateProduct(@RequestBody final Product product) {
+        if (productService.findById(product.getId()) == null) {
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        }
+        productService.save(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        if (product == null) {
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        }
+        productService.delete(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
 }
